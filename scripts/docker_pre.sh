f5b1d4e66e8e973858a1d892ba7fefaa62ddcd3a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-08-16 12:18:46 +0200

    Use ENV instruction rather than export

--- scripts/docker_pre.sh
+++ scripts/docker_pre.sh
@@ -4,16 +4,12 @@
 #
 # SPDX-License-Identifier: MIT
 
-# Export install prefix
-export INSTALL_PREFIX=/usr/local
-
 # Prepare apt
 export DEBIAN_FRONTEND="noninteractive"
 apt -y -qq update
 
 # Clone FEM on Colab repository
 apt install -y -qq git
-export INSTALL_SCRIPTS="/usr/local/share/fem-on-colab"
 if [ ! -d $INSTALL_SCRIPTS ]; then
     git clone https://github.com/fem-on-colab/fem-on-colab.git $INSTALL_SCRIPTS
 else
@@ -29,6 +25,7 @@ find ${INSTALL_SCRIPTS} -type f -name install.sh -exec truncate -s 0 {} \;
 apt install -y -qq python3
 export PYTHON_VERSION=$(python3 -c "import sys; print('python{}.{}'.format(sys.version_info.major, sys.version_info.minor))")
 ln -s ${INSTALL_PREFIX}/lib/${PYTHON_VERSION} ${INSTALL_PREFIX}/lib/python3.7
+ln -fs ${INSTALL_PREFIX}/lib/${PYTHON_VERSION} ${INSTALL_PREFIX}/lib/python3
 
 # Increase verbosity
 export VERBOSE=1
